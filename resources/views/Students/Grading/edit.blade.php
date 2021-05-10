19ee4700c9bcd9d10be84fe4d372de8b4594f9ee
ivancy21 <rexivancy21@gmail.com>
2021-05-10 21:59:13 +0800

    addJsGrading

--- resources/views/Students/Grading/edit.blade.php
+++ resources/views/Students/Grading/edit.blade.php
@@ -45,12 +45,12 @@
                             <input type="hidden" name="grades[{{$count}}][gradeLevel]" value="{{$enrolled->enrolled->gradeLevel}}">
                             <input type="hidden" name="grades[{{$count}}][subject]" value="{{$studentGrades->subject}}">
                             <td class="text-left align-middle">{{$studentGrades->subject}}</td>
-                            <td><input type="number" class="form-control" name="grades[{{$count}}][firstQuarter]" value="{{$studentGrades->firstQuarter ?? '0'}}"></td>
-                            <td><input type="number" class="form-control" name="grades[{{$count}}][secondQuarter]" value="{{$studentGrades->secondQuarter ?? '0'}}"></td>
-                            <td><input type="number" class="form-control" name="grades[{{$count}}][thirdQuarter]" value="{{$studentGrades->thirdQuarter ?? '0'}}"></td>
-                            <td><input type="number" class="form-control" name="grades[{{$count}}][fourthQuarter]" value="{{$studentGrades->fourthQuarter ?? '0'}}"></td>
-                            <td class="align-middle">0</td>
-                            <td class="align-middle"></td>
+                            <td><input type="number" class="form-control firstQuarter" name="grades[{{$count}}][firstQuarter]" onchange="firstQuarter(this)" value="{{$studentGrades->firstQuarter}}"></td>
+                            <td><input type="number" class="form-control secondQuarter" name="grades[{{$count}}][secondQuarter]" onchange="secondQuarter(this)" value="{{$studentGrades->secondQuarter}}"></td>
+                            <td><input type="number" class="form-control thirdQuarter" name="grades[{{$count}}][thirdQuarter]" onchange="thirdQuarter(this)" value="{{$studentGrades->thirdQuarter}}"></td>
+                            <td><input type="number" class="form-control fourthQuarter" name="grades[{{$count}}][fourthQuarter]" onchange="getAverage(this)" value="{{$studentGrades->fourthQuarter}}"></td>
+                            <td class="align-middle average"></td>
+                            <td class="align-middle"><span class="remarks" style="color: #8cbd01"></span></td>
                             {{-- <td class="align-middle"><span style="color: #8cbd01">Passed</span></td> --}}
                         </tr>
                     @endforeach
@@ -66,5 +66,124 @@
     </form>
     </div>
 </div>
+
+
+<script>
+
+    $(document).ready(function(){
+
+        $('.firstQuarter').each(function(){
+            if($(this).val() != ""){
+               $(this).closest('tr').find('.secondQuarter').attr('readonly', false);
+            }else{
+                $(this).closest('tr').find('.secondQuarter').attr('readonly', true);
+            }
+
+
+        var second = parseFloat($(this).closest('tr').find('.secondQuarter').val());  
+        var third =  parseFloat($(this).closest('tr').find('.thirdQuarter').val());  
+        var fourth =  parseFloat($(this).closest('tr').find('.fourthQuarter').val());  
+            var first =  parseFloat($(this).closest('tr').find('.firstQuarter').val()); 
+
+            var total = (first + second + third + fourth) / 4;
+
+            $(this).closest('tr').find('.average').html(total);
+
+      
+            if(total >= 74.5){
+                $(this).closest('tr').find('.remarks').html('Passed');
+            }else if(total <= 74.4){
+                $(this).closest('tr').find('.remarks').html('Failed');
+            }else{
+            }
+
+
+
+        });
+
+        $('.secondQuarter').each(function(){
+            if($(this).val() != ""){
+                $(this).closest('tr').find('.thirdQuarter').attr('readonly', false);
+            }else{
+                $(this).closest('tr').find('.thirdQuarter').attr('readonly', true);
+            }
+        });
+
+        $('.thirdQuarter').each(function(){
+            if($(this).val() != ""){
+                $(this).closest('tr').find('.fourthQuarter').attr('readonly', false);
+            }else{
+                $(this).closest('tr').find('.fourthQuarter').attr('readonly', true);
+            }
+        });
+
+
+
+
+
+
+    });
+
+
+
+    function firstQuarter(thisInput){
+        if($(thisInput).val() != ""){
+            $(thisInput).closest('tr').find('.secondQuarter').attr('readonly', false);
+        }else{
+            $(thisInput).closest('tr').find('.secondQuarter').val('');  
+            $(thisInput).closest('tr').find('.thirdQuarter').val('');  
+            $(thisInput).closest('tr').find('.fourthQuarter').val('');  
+            $(thisInput).closest('tr').find('.secondQuarter').attr('readonly', true);
+            $(thisInput).closest('tr').find('.thirdQuarter').attr('readonly', true);
+            $(thisInput).closest('tr').find('.fourthQuarter').attr('readonly', true);
+        }
+        getAverage(thisInput);
+    }
+
+    function secondQuarter(thisInput){
+        if($(thisInput).val() != ""){
+            $(thisInput).closest('tr').find('.thirdQuarter').attr('readonly', false);
+        }else{
+            $(thisInput).closest('tr').find('.thirdQuarter').val('');  
+            $(thisInput).closest('tr').find('.fourthQuarter').val('');  
+            $(thisInput).closest('tr').find('.thirdQuarter').attr('readonly', true);
+            $(thisInput).closest('tr').find('.fourthQuarter').attr('readonly', true);
+        }
+        getAverage(thisInput);
+    }
+
+    function thirdQuarter(thisInput){
+        if($(thisInput).val() != ""){
+            $(thisInput).closest('tr').find('.fourthQuarter').attr('readonly', false);
+        }else{
+            $(thisInput).closest('tr').find('.fourthQuarter').val('');  
+            $(thisInput).closest('tr').find('.fourthQuarter').attr('readonly', true);
+        }
+
+        getAverage(thisInput);
+    }
+
+
+    function getAverage(thisInput){
+        var second = parseFloat($(thisInput).closest('tr').find('.secondQuarter').val());  
+        var third =  parseFloat($(thisInput).closest('tr').find('.thirdQuarter').val());  
+        var fourth =  parseFloat($(thisInput).closest('tr').find('.fourthQuarter').val());  
+            var first =  parseFloat($(thisInput).closest('tr').find('.firstQuarter').val()); 
+
+            var total = (first + second + third + fourth) / 4;
+
+            $(thisInput).closest('tr').find('.average').html(total);
+
+      
+            if(total >= 74.5){
+                $(thisInput).closest('tr').find('.remarks').html('Passed');
+            }else if(total <= 74.4){
+                $(thisInput).closest('tr').find('.remarks').html('Failed');
+            }else{
+            }
+            
+    }
+
+</script>
 
 @endsection
